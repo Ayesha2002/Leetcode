a094dcb8e02359cd6ecc0f612da3de647d82966a
Ayesha Khan <91973186+Ayesha2002@users.noreply.github.com>
2024-08-16 23:55:04 +0530

    Create (16-08)624. Maximum Distance in Arrays.cpp

---
+++ August/(16-08)624. Maximum Distance in Arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int min= arrays[0][0];
+        int max= arrays[0].back();
+        int maxDistance=0;
+
+        for (int i=1;i<arrays.size();i++){
+            maxDistance = max(maxDistance, max(abs(arrays[i].back() - minVal), abs(maxVal - arrays[i][0])));
+        }
+    }
+};
