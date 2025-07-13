74af7b97dc5885a35058cc8ba25a91632001e91d
Ayesha Khan <91973186+Ayesha2002@users.noreply.github.com>
2025-07-13 22:41:27 +0530

    Create 2410. Maximum Matching of Players With Trainers.cpp

---
+++ July(2025)/2410. Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,27 @@
+//Time complexity: O(mlog m +n log n)
+//Space complexity:O(1)
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(begin(players), end(players));
+        sort(begin(trainers), end(trainers));
+
+        int m=players.size();
+        int n=trainers.size();
+
+        int count=0;
+        int i=0;
+        int j=0;
+
+        while(i<m && j<n){
+            if(players[i] <= trainers[j]){
+                i++;
+                count++;
+            }
+            j++;
+        }
+        return count;
+
+    }
+};
